4ee2d1907fb2a5ca1c3a41087e6fddde2329cf1a
Sarthak <sarthakteotia31@gmail.com>
2025-09-20 00:53:38 +0530

    Last commit

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ function App() {
     
     try {
       const response = await axios({
-        url:"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyCBh6oPq1XhUCLD8iRrWyhqSSWtaiHrWZM",
+        url:`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${import.meta.env.VITE_GEMINI_API_KEY}`,
         method:"POST",
         data:{
           "contents": [
